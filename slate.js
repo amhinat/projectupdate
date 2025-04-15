35c2d104dd0ed6ad2b20472619d6e5ba9a7683e3
amhinat <makojiaminat@gmail.com>
2025-04-15 13:25:49 +0100

    changes made

--- slate.js
+++ slate.js
@@ -1,9 +1,7 @@
-function toggleMenu() {
-    const nav = document.getElementById("hiddenNav");
-    nav.classList.toggle("show-nav");
-}
-event.preventDefault();
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
 
-
-
+hamburger.onclick = () => {
+  navLinks.classList.toggle('active');
+};
 
