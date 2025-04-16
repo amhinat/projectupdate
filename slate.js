a0f853d9aba933b90a0844e7aae8b7ba76224331
amhinat <makojiaminat@gmail.com>
2025-04-16 15:53:44 +0100

    updated

--- slate.js
+++ slate.js
@@ -1,7 +1,7 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navLinks');
+ const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
 
-hamburger.onclick = () => {
-  navLinks.classList.toggle('active');
-};
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
 
