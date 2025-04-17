58e686c1396305adde9edebfbfaf991cd898123f
amhinat <makojiaminat@gmail.com>
2025-04-17 15:31:50 +0100

    commited

--- slate.js
+++ slate.js
@@ -4,4 +4,26 @@
   hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("show");
   });
+  const surveyform =document.getElementById("surveyform");
+  const loadingMessage =document.getElementById("loadingMessage");
 
+  surveyform.addEventListener("Submit", function(event)  { 
+
+    event.preventDefault();
+    showloading();
+
+    setTimeout(() =>{
+      hideloading()
+      alert("message submitted successfully!")
+      surveyform.reset()
+    }, 2000);
+  });
+  function showloading(){
+    surveyform.style.display = "none";
+    loadingMessage.style.display = "block";
+  }
+
+  function hideloading(){
+    loadingMessage.style.display = "none";
+    surveyform.style.display = "block";
+  }
